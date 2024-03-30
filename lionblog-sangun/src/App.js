2622ea6d236411ce8a2d455ad1f6160c1a65695e
Park <gse1818@naver.com>
2024-03-30 17:50:21 +0900

    new file:   lionblog-sangun/src/components/Posts/index.jsx new file:   lionblog-sangun/src/routes/PostCreatePage.jsx new file:   lionblog-sangun/src/routes/PostDetailPage.jsx new file:   lionblog-sangun/src/routes/PostEditPage.jsx new file:   lionblog-sangun/src/routes/SignInPage.jsx

--- lionblog-sangun/src/App.js
+++ lionblog-sangun/src/App.js
@@ -1,10 +1,30 @@
-import Header from "./componenets/Header";
+// 얘가 root 파일
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./routes/HomePage";
+import SignUpPage from "./routes/SignUpPage";
+import SignInPage from "./routes/SignInPage";
+import PostCreatePage from "./routes/PostCreatePage";
+import PostDetailPage from "./routes/PostDetailPage";
+import PostEditPage from "./routes/PostEditPage";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/create" element={<PostCreatePage />} />
+          <Route path="/:postId" element={<PostDetailPage />} />
+          <Route path="/:postId/edit" element={<PostEditPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
