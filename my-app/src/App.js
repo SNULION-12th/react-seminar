76b3a1faa8003fd940cd3022a79b325bdc6c466c
yeonghun <jhun00303@snu.ac.kr>
2024-03-30 17:51:22 +0900

    week4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./routes/HomePage";
+import SignUpPage from "./routes/SignUpPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignInPage from "./routes/SignInPage";
+import PostCreatePage from "./routes/PostCreatePage";
+import PostDetailPage from "./routes/PostDetailPage";
+import PostEditPage from "./routes/PostEditPage";
 
 function App() {
-  return <div className="App">블로그 구현 시작!</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/create" element={<PostCreatePage />} />
+          <Route path="/:postId" element={<PostDetailPage />} />
+          <Route path="/:postId/edit" element={<PostEditPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      {/* <Header />
+      <HomePage />
+      <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
