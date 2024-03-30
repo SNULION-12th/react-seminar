e1bf74ef0a6cb5551dbe28399174e287c98f2948
minss7 <paul2723@snu.ac.kr>
2024-03-30 17:51:08 +0900

    commit

--- lionblogminseok/src/App.js
+++ lionblogminseok/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./routes/HomePage";
+import SignUpPage from "./routes/SignUpPage";
+import SignInPage from "./routes/SignInPage";
+import PostCreatePage from "./routes/PostCreatePage";
+import PostDetailPage from "./routes/PostDetailPage";
+import PostEditPage from "./routes/PostEditPage";
+
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">블로그 구현 시작!</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/create" element={<PostCreatePage />} />
+        <Route path="/:postId" element={<PostDetailPage />} />
+        <Route path="/:postId/edit" element={<PostEditPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
