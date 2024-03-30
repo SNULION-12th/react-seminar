38bd41518ff231f71595cdb7f72dd0fb4c5060c5
sono <rhj080471i@snu.ac.kr>
2024-03-30 16:56:53 +0900

    create : Homepage

--- lionblon-hojun/src/App.js
+++ lionblon-hojun/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./compoents/Footer";
 import Header from "./compoents/Header";
+import HomePage from "./routes/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
