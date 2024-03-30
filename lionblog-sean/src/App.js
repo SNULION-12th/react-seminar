81b7ca0c7b6c5a1dbb1607c31c084034f48f916c
Sean <seahn1021@snu.ac.kr>
2024-03-30 17:50:58 +0900

    feat: seminar

--- lionblog-sean/src/App.js
+++ lionblog-sean/src/App.js
@@ -1,8 +1,30 @@
 import "./App.css";
+import Header from "./components/Header";
+import HomePage from "./routes/HomePage";
+import Footer from "./components/Footer";
+import SignUpPage from "./routes/SignUpPage";
+import PostCreatePage from "./routes/PostCreatePage";
+import PostDetailPage from "./routes/PostDetailPage";
+import PostEditPage from "./routes/PostEditPage";
+import SignInPage from "./routes/SignInPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">블로그 구현 시작!</div>
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/create" element={<PostCreatePage />} />
+          <Route path="/:postId" element={<PostDetailPage />} />
+          <Route path="/:postId/edit" element={<PostEditPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
