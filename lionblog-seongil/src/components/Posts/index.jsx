e7e31b92c5134755a7c4f0a184805d9841043176
Atipico1 <athjk3@snu.ac.kr>
2024-04-01 22:32:52 +0900

    Feat: week4 homework complete

--- lionblog-seongil/src/components/Posts/index.jsx
+++ lionblog-seongil/src/components/Posts/index.jsx
@@ -14,3 +14,27 @@ export const SmallPost = ({ post }) => {
     </div>
   );
 };
+
+export const BigPost = ({ post }) => {
+  const likeBtnClick = () => {
+    alert("좋아요");
+  };
+  return (
+    <div className="w-max flex flex-col justify-center items-center">
+      <div className="block group py-10 px-8 mr-5 my-5 ring-8 ring-transparent border-2 border-box border-white hover:bg-orange-400 hover:text-black hover:border-transparent hover:ring-orange-200 rounded-xl font-medium">
+        <h1 className="font-extrabold text-2xl truncate">{post.title}</h1>
+        <p className="mt-2">{post.author.username}</p>
+        <div className="flex flex-wrap mt-5">
+          {post.tags.map((tag) => (
+            <span key={tag.id} className="tag m-1">
+              #{tag.content}
+            </span>
+          ))}
+        </div>
+        <div onClick={likeBtnClick}>
+          {post.like_users.length > 0 && `❤️ ${post.like_users.length}`}
+        </div>
+      </div>
+    </div>
+  );
+};
