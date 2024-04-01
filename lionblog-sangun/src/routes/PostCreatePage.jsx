e7d61512d5c20726187f65365b259e5be79eaf61
Park <gse1818@naver.com>
2024-04-02 02:43:30 +0900

    modified:   lionblog-sangun/src/components/Posts/index.jsx modified:   lionblog-sangun/src/index.js modified:   lionblog-sangun/src/routes/PostCreatePage.jsx modified:   lionblog-sangun/src/routes/PostDetailPage.jsx modified:   lionblog-sangun/src/routes/PostEditPage.jsx modified:   lionblog-sangun/src/routes/SignInPage.jsx

--- lionblog-sangun/src/routes/PostCreatePage.jsx
+++ lionblog-sangun/src/routes/PostCreatePage.jsx
@@ -1,5 +1,49 @@
 const PostCreatePage = () => {
-  return <div>Post Create Page</div>;
+  const handleCreateSubmit = () => {
+    alert("게시글을 등록합니다"); // TODO: add api call for sign in
+  };
+  return (
+    <div className="flex flex-col items-center w-1/2">
+      <h3 className="font-bold text-2xl">게시글 작성</h3>
+      <form className="form gap-2" onSubmit={handleCreateSubmit}>
+        <label required htmlFor="title" className="label">
+          제목:
+        </label>
+        <input
+          required
+          type="text"
+          id="title"
+          className="input"
+          placeholder="제목을 입력하세요"
+        />
+
+        <label htmlFor="content" className="label">
+          내용:
+        </label>
+        <textarea
+          required
+          type="text"
+          id="content"
+          className="input h-56"
+          placeholder="내용을 입력하세요"
+        />
+
+        <label htmlFor="tag" className="label">
+          태그:
+        </label>
+        <div className="flex w-full">
+          <input id="tag" className="input" placeholder="태그를 추가하세요" />
+          <button className="w-20">add</button>
+        </div>
+
+        <div className="flex flex-row items-center gap-5">
+          <button type="submit" className="button mt-7">
+            완료
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PostCreatePage;
--- lionblog-sangun/src/routes/PostDetailPage.jsx
+++ lionblog-sangun/src/routes/PostDetailPage.jsx
@@ -1,5 +1,38 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BigPost } from "../components/Posts";
+import posts from "../data/posts";
+import { Link } from "react-router-dom";
+
 const PostDetailPage = () => {
-  return <div>Post Detail Page</div>;
+  const postId = useParams().postId;
+
+  const [post, setPost] = useState(null);
+  useEffect(() => {
+    const post = posts.find((post) => post.id === parseInt(postId));
+    setPost(post);
+  }, [postId]);
+
+  const handleDeleteButtonClick = () => {
+    alert("삭제 버튼을 클릭했습니다!");
+    // TODO: 실제 삭제 동작 수행
+  };
+
+  return (
+    post && (
+      <div className="flex flex-col items-center w-1/2">
+        <BigPost key={post.id} post={post} />
+        <div className="flex flex-row items-center gap-5">
+          <Link to={`/${post.id}/edit`} className="button mt-7">
+            수정
+          </Link>
+          <button onClick={handleDeleteButtonClick} className="button mt-7">
+            삭제
+          </button>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default PostDetailPage;
