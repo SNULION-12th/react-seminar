9b818e4b983362be34665110d1219766850aea31
minseo25 <ms059333@gmail.com>
2024-04-02 11:16:56 +0900

    week4 hw complete!

--- lionblog-minseo/src/components/Posts/index.jsx
+++ lionblog-minseo/src/components/Posts/index.jsx
@@ -1,6 +1,8 @@
-export const SmallPost = ({ post }) => {
+import { Link } from 'react-router-dom';
+
+const SmallPost = ({ post }) => {
   return (
-    <div className="w-64 relative block group py-10 px-8 mr-5 my-5 ring-8 ring-transparent border-2 border-box border-white hover:bg-orange-400 hover:text-black hover:border-transparent hover:ring-orange-200 rounded-xl font-medium">
+    <div className="w-64 h-64 relative block group py-10 px-8 mr-5 my-5 ring-8 ring-transparent border-2 border-box border-white hover:bg-orange-400 hover:text-black hover:border-transparent hover:ring-orange-200 rounded-xl font-medium">
       <h1 className="font-extrabold text-2xl truncate">{post.title}</h1>
       <p className="mt-2">{post.author.username}</p>
       <div className="flex flex-wrap mt-5">
@@ -16,3 +18,40 @@ export const SmallPost = ({ post }) => {
     </div>
   );
 };
+const BigPost = ({ post }) => {
+  const handleBigPostLike = () => {
+    alert("좋아요 누름");
+  };
+  const handleBigPostDelete = () => {
+    alert("삭제 누름");
+  };
+  return (
+    <div className="w-screen flex align-center justify-center">
+    <div className="w-4/5 mt-8 flex flex-col items-center justify-center">
+      <div className="w-full max-w-none relative block group py-10 px-8 mr-5 my-5 ring-8 ring-transparent border-1 border-box border-white bg-orange-400 text-black border-transparent ring-orange-200 rounded-xl font-medium">
+        <div className="flex flex-row justify-between mb-5">
+          <h1 className="font-extrabold text-2xl truncate inline">{post.author.username}의 {post.title}</h1>
+          <span>{post.created_at.substring(0, 10)}</span>
+        </div>
+        <div className="border border-solid border-black rounded-md p-3">{post.content}</div>
+        <div className="flex flex-wrap mt-5">
+          {post.tags.map((tag) => (
+            <span key={tag.id} className="tag m-1">
+              #{tag.content}
+            </span>
+          ))}
+        </div>
+        <div className="hover:cursor-pointer" onClick={handleBigPostLike}>
+          {post.like_users.length > 0 && `🖤 ${post.like_users.length}`}
+        </div>
+      </div>
+      <div className="flex flex-row items-center gap-5 justify-center mt-3 mb-7 w-full">
+				<Link to={`/${post.id}/edit`} key={post.id}><button className="button mt-7">수정</button></Link>
+				<button onClick={handleBigPostDelete} className="button mt-7">삭제</button>
+			</div>
+    </div>
+    </div>
+  );
+};
+
+export { SmallPost, BigPost };
--- lionblog-minseo/src/routes/PostCreatePage.jsx
+++ lionblog-minseo/src/routes/PostCreatePage.jsx
@@ -1,6 +1,28 @@
 const PostCreatePage = () => {
+	const handlePostCreateSubmit = () => {
+    alert("게시물을 등록합니다");
+  };
 	return (
-		<div>Post Create Page</div>
+		<div className="flex flex-col items-center w-3/4">
+			<h3 className="font-bold text-2xl">게시물 작성</h3>
+			<form className="form gap-2" onSubmit={handlePostCreateSubmit}>
+				<label required htmlFor="title" className="label">제목</label>
+        <input required type="text" id="title" className="input" placeholder="제목을 입력하세요" />
+
+        <label htmlFor="content" className="label">내용</label>
+        <textarea required type="text" id="content" className="input" rows="9" placeholder="내용을 입력하세요" />
+
+				<label htmlFor="college" className="label">태그</label>
+				<div className="w-full flex flex-row">
+					<input type="text" id="college" className="input" placeholder="태그를 추가하세요" />
+					<div className="w-24 ml-2 flex items-center justify-center hover:cursor-pointer">추가</div>
+				</div>
+
+        <div className="flex flex-row items-center gap-5">
+					<button type="submit" className="button mt-7">완료</button>
+        </div>
+	    </form>
+		</div>
 	);
 };
 
