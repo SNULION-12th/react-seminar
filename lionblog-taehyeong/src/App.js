5660cc1670659ff2381f6ed68ceeb99681e81c92
Taehyeungkim <kth1837@snu.ac.kr>
2024-04-03 23:35:23 +0900

    :sparkles: added dark/light mode with useContext

--- lionblog-taehyeong/src/App.js
+++ lionblog-taehyeong/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createContext, useState } from "react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -10,22 +11,30 @@ import PostDetailPage from "./routes/PostDetailPage";
 import PostEditPage from "./routes/PostEditPage";
 import "./App.css";
 
+export const DarkModeContext = createContext(false);
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const darkModeToggle = () => setDarkMode(!darkMode);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/create" element={<PostCreatePage />} />
-          <Route path="/:postId" element={<PostDetailPage />} />
-          <Route path="/:postId/edit" element={<PostEditPage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <DarkModeContext.Provider value={darkMode}>
+      <div className={`App ${darkMode ? "dark" : "light"}`}>
+        <BrowserRouter>
+          <Header darkModeToggle={darkModeToggle} />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/create" element={<PostCreatePage />} />
+            <Route path="/:postId" element={<PostDetailPage />} />
+            <Route path="/:postId/edit" element={<PostEditPage />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </DarkModeContext.Provider>
   );
 }
 
--- lionblog-taehyeong/src/components/Posts/index.jsx
+++ lionblog-taehyeong/src/components/Posts/index.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../../App";
 
 export const SmallPost = ({ post }) => {
+  const darkMode = useContext(DarkModeContext);
   return (
     <Link
       to={`/${post.id}`}
-      className="w-64 relative block group py-10 px-8 mr-5 my-5 ring-8 ring-transparent border-2 border-box border-white hover:bg-orange-400 hover:text-black hover:border-transparent hover:ring-orange-200 rounded-xl font-medium"
+      className={`post-transition w-64 relative block group py-10 px-8 mr-5 my-5 ring-8 ring-transparent border-2 border-box hover:bg-orange-400 hover:text-black hover:border-transparent hover:ring-orange-200 rounded-xl font-medium ${
+        darkMode ? "border-white" : "border-black"
+      }`}
     >
       <h1 className="font-extrabold text-2xl truncate">{post.title}</h1>
       <p className="mt-2">{post.author.username}</p>
