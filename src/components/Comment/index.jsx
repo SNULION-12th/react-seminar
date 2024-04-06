103879ac3aeb4d8572b2546731c70ee1a5c778ee
hoyah <hoyaho03@snu.ac.kr>
2024-04-06 16:36:33 +0900

    test

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import comments from "../../data/comments"; // dummy data
+import CommentElement from "./CommentElement";
+
+const Comment = ({ postId }) => {
+  // TODO: comments를 저장하기 위한 state를 만들어주세요
+  // TODO: 새로운 댓글을 추가하기 위한 state를 만들어주세요
+
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+    alert("댓글 작성"); // add api call for creating comment
+    // TODO: 댓글을 추가했으니 새로운 댓글을 저장하는 state를 초기화해야겠죠?
+  };
+
+  const handleCommentDelete = (commentId) => {
+    console.log(commentId);
+    alert("댓글 삭제"); // add api call for deleting comment
+  };
+
+  return (
+    <div className="w-full mt-5 self-start">
+      <h1 className="text-3xl font-bold my-5">Comments</h1>
+      // TODO: comments 더미데이터를 돌며 각 댓글마다 CommentElement를
+      만들어주세요! // Hint: CommentElement에는 댓글인 comment와 댓글 삭제를
+      하는 handleCommentDelete라는 props가 필요합니다. // TODO: 새로운 댓글을
+      작성하는 form을 만들어주세요!
+      <form className="flex flex-row items-center justify-center mt-10 gap-2"></form>
+    </div>
+  );
+};
+
+export default Comment;
