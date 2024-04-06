f6e41d3305dab659a4218cc1bca9863f851d5265
이예은 <gkfn1121123@gmail.com>
2024-04-06 16:37:32 +0900

    Initial Commit

--- src/routes/PostDetailPage.jsx
+++ src/routes/PostDetailPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { BigPost } from "../components/Posts";
 import posts from "../data/posts";
 
@@ -11,11 +11,12 @@ const PostDetailPage = () => {
     setPost(post);
   }, [postId]);
 
+  const navigate = useNavigate();
   const onClickDelete = () => {
-    alert("삭제");
-    //TODO : api connect(delete post)
+    alert("게시물을 삭제합니다.");
+    navigate("/");
+    // add api call for deleting post
   };
-
   return (
     post && (
       <div className="flex flex-col items-center w-[60%] p-8">
