45b59547e81d80216365e0937c7ef83bb0db6ae8
Taehyeungkim <kth1837@snu.ac.kr>
2024-04-08 20:17:39 +0900

    :art: finished comment edit

--- lionblog-taehyeong/src/components/Comment/CommentElement.jsx
+++ lionblog-taehyeong/src/components/Comment/CommentElement.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 const CommentElement = ({ comment, handleCommentDelete }) => {
   const [isEditMode, setIsEditMode] = useState(false);
+  const [content, setContent] = useState(comment.content);
 
   /* TODO: 댓글을 수정하는 input의 value를 관리하기 위한 state 작성
        Hint: 댓글의 내용을 저장하는 state와 수정 중인지 여부를 저장하는 state를 따로 만드는 게 좋겠죠? */
@@ -14,8 +15,9 @@ const CommentElement = ({ comment, handleCommentDelete }) => {
   let day = date.getDate();
   day = day < 10 ? `0${day}` : day;
 
-  const handleEditComment = () => {
-    alert("댓글 수정"); // add api call for editing comment
+  const handleEditComment = (e) => {
+    e.preventDefault();
+    setIsEditMode(false);
   };
 
   const toEditMode = (e) => {
@@ -36,9 +38,13 @@ const CommentElement = ({ comment, handleCommentDelete }) => {
     <div className="w-full flex flex-row justify-between items-center mb-5">
       <div className="w-3/4 flex flex-col gap-1">
         {isEditMode ? (
-          <input className="input" defaultValue={comment.content} />
+          <input
+            className="input"
+            defaultValue={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
         ) : (
-          <p>{comment.content}</p>
+          <p>{content}</p>
         )}
 
         <span className="text-base text-gray-300">
@@ -52,7 +58,9 @@ const CommentElement = ({ comment, handleCommentDelete }) => {
             <button className="button-no-bg" onClick={toShowMode}>
               취소
             </button>
-            <button className="button-no-bg">완료 </button>
+            <button className="button-no-bg" onClick={handleEditComment}>
+              완료{" "}
+            </button>
           </>
         ) : (
           <>
