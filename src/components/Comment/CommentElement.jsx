18f55bda848555584e2773c89ce70245db8d5f1b
이예은 <gkfn1121123@gmail.com>
2024-04-09 10:21:57 +0900

    fix: comment edit

--- src/components/Comment/CommentElement.jsx
+++ src/components/Comment/CommentElement.jsx
@@ -1,16 +1,14 @@
 import { useState, useEffect } from "react";
-import comments from "../../data/comments";
 
-const CommentElement = ({ comment, handleCommentDelete }) => {
-  console.log(comment);
-  console.log(typeof handleCommentDelete);
+const CommentElement = ({ comment, handleCommentDelete, handleCommentEdit }) => {
+
   /* TODO: props 받기
        Hint: src/components/Comment/index.jsx에서 어떠한 props를 넘겨주는지 확인해보세요! */
 
   /* TODO: 댓글을 수정하는 input의 value를 관리하기 위한 state 작성
        Hint: 댓글의 내용을 저장하는 state와 수정 중인지 여부를 저장하는 state를 따로 만드는 게 좋겠죠? */
   const [isEditing, setIsEditing] = useState(false);
-  const [editComment, setEditComment] = useState(comment.content);
+  const [editedComment, setEditedComment] = useState(comment.content);
 
   // comment created_at 전처리
   const date = new Date(comment.created_at);
@@ -19,10 +17,6 @@ const CommentElement = ({ comment, handleCommentDelete }) => {
   month = month < 10 ? `0${month}` : month;
   let day = date.getDate();
   day = day < 10 ? `0${day}` : day;
-
-  const handleEditComment = () => {
-    alert("댓글 수정"); // add api call for editing comment
-  };
 
   useEffect(() => {
     // add api call to check if user is the author of the comment
@@ -36,8 +30,8 @@ const CommentElement = ({ comment, handleCommentDelete }) => {
         {isEditing ? (
           <input
             className="input"
-            onChange={(e) => setEditComment(e.target.value)}
-            value={editComment}
+            onChange={(e) => setEditedComment(e.target.value)}
+            value={editedComment}
           ></input>
         ) : (
           <p>{comment.content}</p>
@@ -61,6 +55,7 @@ const CommentElement = ({ comment, handleCommentDelete }) => {
             <button
               onClick={() => {
                 setIsEditing(false);
+                handleCommentEdit(comment.id, editedComment);
               }}
             >
               완료
@@ -77,7 +72,7 @@ const CommentElement = ({ comment, handleCommentDelete }) => {
             </button>
             <button
               onClick={() => {
-                setEditComment([]);
+                setEditedComment([]);
                 handleCommentDelete(comment.id);
               }}
             >
--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -21,14 +21,21 @@ const Comment = ({ postId }) => {
   };
 
   const handleCommentDelete = (commentId) => {
-    console.log(commentId);
     setCommentList(commentList.filter((comment) => comment.id !== commentId));
     alert("댓글 삭제"); // add api call for deleting comment
   };
 
-  const tmp = comments.filter((c) => c.id === 1);
-
-  console.log(tmp);
+  const handleCommentEdit = (commentId, editedComment) => {
+    setCommentList((commentList) => {
+      return commentList.map((comment) => {
+        if (comment.id === commentId) {
+          return { ...comment, content: editedComment };
+        }
+        return comment;
+      });
+    });
+    alert("댓글 수정");
+  };
 
   return (
     <div className="w-full mt-5 self-start">
@@ -37,18 +44,22 @@ const Comment = ({ postId }) => {
         <CommentElement
           comment={comment}
           handleCommentDelete={handleCommentDelete}
+          handleCommentEdit={handleCommentEdit}
         />
       ))}
       <form
         onSubmit={handleCommentSubmit}
         className="flex flex-row items-center justify-center mt-10 gap-2"
       >
-    
-        <input required className="input grow"
+        <input
+          required
+          className="input"
           value={newComment}
           onChange={(e) => setNewComment(e.target.value)}
         ></input>
-        <button type="submit" className="button w-24 h-12 py-10">작성</button>
+        <button type="submit" className="button w-24 h-12 py-10">
+          작성
+        </button>
       </form>
     </div>
   );
