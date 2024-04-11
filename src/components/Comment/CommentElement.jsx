25672574ee685f53871821cbbf1cf99df19b530d
yebin <yebinp1926@gmail.com>
2024-04-11 14:56:42 +0900

    yebin commit 2

--- src/components/Comment/CommentElement.jsx
+++ src/components/Comment/CommentElement.jsx
@@ -1,42 +1,74 @@
 import { useState, useEffect } from "react";
 
-const CommentElement = (props) => {
-    /* TODO: props 받기
+const CommentElement = ({ comment, handleCommentDelete, postId }) => {
+  /* TODO: props 받기
        Hint: src/components/Comment/index.jsx에서 어떠한 props를 넘겨주는지 확인해보세요! */
 
-    /* TODO: 댓글을 수정하는 input의 value를 관리하기 위한 state 작성
+  /* TODO: 댓글을 수정하는 input의 value를 관리하기 위한 state 작성
        Hint: 댓글의 내용을 저장하는 state와 수정 중인지 여부를 저장하는 state를 따로 만드는 게 좋겠죠? */
+  const [commentContent, setCommentContent] = useState(comment.content);
+  const [isEditing, setIsEditing] = useState(false);
 
-    // comment created_at 전처리
-    const date = new Date(comment.created_at);
-    const year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    month = month < 10 ? `0${month}` : month;
-    let day = date.getDate();
-    day = day < 10 ? `0${day}` : day;
+  // comment created_at 전처리
+  const date = new Date(comment.created_at);
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  month = month < 10 ? `0${month}` : month;
+  let day = date.getDate();
+  day = day < 10 ? `0${day}` : day;
 
-    const handleEditComment = () => {
-        alert("댓글 수정"); // add api call for editing comment
-    };
+  const handleEditComment = () => {
+    alert("댓글 수정"); // add api call for editing comment
+  };
 
-    useEffect(() => { // add api call to check if user is the author of the comment
-    }, []);
+  useEffect(() => {
+    // add api call to check if user is the author of the comment
+  }, []);
 
-    return (
-        <div className="w-full flex flex-row justify-between items-center mb-5">
-            <div className="w-3/4 flex flex-col gap-1">
-                // TODO: 수정 중일 때와 아닐 때를 나눠서 보여줘야 해요!
-                {수정 중 ? <input ... /> : <p ...>댓글 내용</p>}
+  return (
+    <div className="w-full flex flex-row justify-between items-center mb-5 text-sm">
+      <div className="w-3/4 flex flex-col gap-1">
+        {
+          // TODO: 수정 중일 때와 아닐 때를 나눠서 보여줘야 해요!
+        }
+        {isEditing ? (
+          <input
+            defaultValue={comment.content}
+            className="border-2 border-orange-400 bg-black text-white p-3 rounded-2xl"
+          />
+        ) : (
+          <p className="bg-black text-white">{comment.content}</p>
+        )}
+        {
+          //날짜
+        }
+        <span className="text-base text-gray-300">
+          {year}.{month}.{day}
+        </span>
+      </div>
 
-                // 날짜
-                <span className="text-base text-gray-300">{year}.{month}.{day}</span>
-						</div>
-
-						<div className="flex flex-row items-center gap-3">
-                // TODO: 버튼 (수정 중인 경우와 아닌 경우 나타나는 버튼이 달라요!)
-                {수정 중 ? 취소, 완료 버튼 : 수정, 삭제 버튼}
-            </div>
-        </div>
-    );
+      <div className="flex flex-row items-center gap-3">
+        {isEditing ? (
+          <div>
+            <button className="p-2" onClick={() => setIsEditing(false)}>
+              취소
+            </button>
+            <button className="p-2" onClick={() => setIsEditing(false)}>
+              완료
+            </button>
+          </div>
+        ) : (
+          <div>
+            <button className="p-2" onClick={handleCommentDelete}>
+              삭제
+            </button>
+            <button className="p-2" onClick={() => setIsEditing(true)}>
+              수정
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default CommentElement;
--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,35 +1,83 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import comments from "../../data/comments"; // dummy data
 import CommentElement from "./CommentElement";
 
 const Comment = ({ postId }) => {
-    // TODO: comments를 저장하기 위한 state를 만들어주세요
-    // TODO: 새로운 댓글을 추가하기 위한 state를 만들어주세요
+  // TODO: comments를 저장하기 위한 state를 만들어주세요
+  // TODO: 새로운 댓글을 추가하기 위한 state를 만들어주세요
+  const [commentsList, setCommentsList] = useState(comments);
+  const [newComment, setNewComment] = useState("");
 
-    const handleCommentSubmit = (e) => {
-        e.preventDefault();
-        alert("댓글 작성"); // add api call for creating comment
-        // TODO: 댓글을 추가했으니 새로운 댓글을 저장하는 state를 초기화해야겠죠?
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+    // TODO: 댓글을 추가했으니 새로운 댓글을 저장하는 state를 초기화해야겠죠?
+    const newCommentValue = e.target.elements.content.value;
+    setNewComment(newCommentValue);
+
+    const createdComment = {
+      id: commentsList.length,
+      content: newCommentValue,
+      created_at: new Date(),
+      post: postId,
+      author: {
+        id: 8,
+        username: "아기사자",
+      },
     };
+    setCommentsList((prevCommentsList) => [
+      ...prevCommentsList,
+      createdComment,
+    ]);
 
-    const handleCommentDelete = (commentId) => {
-				console.log(commentId);
-        alert("댓글 삭제"); // add api call for deleting comment
-    };
+    alert("댓글 작성"); // add api call for creating comment
+  };
 
-    return (
-        <div className="w-full mt-5 self-start">
-            <h1 className="text-3xl font-bold my-5">Comments</h1>
-                // TODO: comments 더미데이터를 돌며 각 댓글마다 CommentElement를 만들어주세요!
-                // Hint: CommentElement에는 댓글인 comment와 댓글 삭제를 하는 handleCommentDelete라는 props가 필요합니다.
-                // Hint: CommentElement에는 이후 API 연결을 위한 postId 정보도 필요하겠죠?
-            
-            // TODO: 새로운 댓글을 작성하는 form을 만들어주세요!
-            <form className="flex flex-row items-center justify-center mt-10 gap-2" ...>
-                ...
-            </form>
-        </div>
-    );
+  const handleCommentDelete = (commentId) => {
+    console.log(commentId);
+    alert("댓글 삭제"); // add api call for deleting comment
+    setCommentsList({
+      ...commentsList,
+      id: commentsList.id.filter((idd) => idd !== commentId),
+    });
+  };
+
+  return (
+    <div className="w-full mt-5 self-start">
+      <h1 className="text-3xl font-bold my-5">Comments</h1>
+      {
+        // TODO: comments 더미데이터를 돌며 각 댓글마다 CommentElement를 만들어주세요!
+        // Hint: CommentElement에는 댓글인 comment와 댓글 삭제를 하는 handleCommentDelete라는 props가 필요합니다.
+        // Hint: CommentElement에는 이후 API 연결을 위한 postId 정보도 필요하겠죠?
+      }
+      {commentsList.map((comment) => (
+        <CommentElement
+          comment={comment}
+          handleCommentDelete={handleCommentDelete}
+          postId={postId}
+        ></CommentElement>
+      ))}
+      {
+        // TODO: 새로운 댓글을 작성하는 form을 만들어주세요!
+      }
+      <form
+        className="flex flex-row items-center justify-center mt-10 gap-2"
+        onSubmit={handleCommentSubmit}
+      >
+        <input
+          id="content"
+          type="text"
+          placeholder="댓글을 입력해주세요"
+          className=" flex items-left w-11/12 p-3 px-6 rounded-2xl bg-black text-white border border-white text-sm"
+        ></input>
+        <button
+          className="bg-orange-400 text-white py-3 px-6 w-20 rounded-xl text-sm"
+          type="submit"
+        >
+          작성
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Comment;
