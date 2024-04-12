1ddf5fb7b79eb5edf27bfdaef07afdba08bac073
minseo25 <ms059333@gmail.com>
2024-04-13 02:00:36 +0900

    week5 additional changes

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -8,30 +8,24 @@ const Comment = ({ postId }) => {
     // 댓글 id 관리하기 위한 state 추가했습니다. 댓글 삭제 시 id 중복 방지 위해
     const [commentList, setCommentList] = useState(comments);
     const [commentId, setCommentId] = useState(commentList.length+1);
-    const [newComment, setNewComment] = useState({
-        id: commentId,
-        content: "",
-        created_at: new Date().toISOString(),
-        post: postId,
-        author: { id: 99, username: "아기사자" },   // 아기사지의 UID는 99 가정.
-    });
+    const [newComment, setNewComment] = useState("");
 
     const handleChange = (e) => {
-        setNewComment({ ...newComment, content: e.target.value });
+        setNewComment(e.target.value);
     };
     const handleCommentSubmit = (e) => {
         e.preventDefault();
         // alert("댓글 작성");
         // 댓글을 추가했으니 새로운 댓글을 저장하는 state를 초기화
-        setCommentList([...commentList, newComment]);
-        // set한 state를 set하는데 또 사용하면 비동기화 문제가 발생할 수 있음...
-        setNewComment({
+        setCommentList([...commentList, {
             id: commentId+1,
-            content: "",
+            content: newComment,
             created_at: new Date().toISOString(),
             post: postId,
             author: { id: 99, username: "아기사자" },
-        });
+        }]);
+        // set한 state를 set하는데 또 사용하면 비동기화 문제가 발생할 수 있음...
+        setNewComment("");
         setCommentId(commentId+1);
     };
     const handleCommentEdit = (commentId, editContent) => {
@@ -73,7 +67,7 @@ const Comment = ({ postId }) => {
             ))}
 
             <form className="flex flex-row items-center justify-center mt-10 gap-2" onSubmit={handleCommentSubmit}>
-                <input className="input" onChange={handleChange} value={newComment.content} />
+                <input className="input" onChange={handleChange} value={newComment} />
                 <button className="button py-2 px-10 h-14 whitespace-nowrap">작성</button>
             </form>
         </div>
