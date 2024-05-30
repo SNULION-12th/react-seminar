ae87f1490c3cc5ebd03ca7eeaaa69b7e0de6d349
yebin <yebinp1926@gmail.com>
2024-05-30 21:54:33 +0900

    get comment, create comment

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,40 +1,27 @@
 import { useEffect, useState } from "react";
-import comments from "../../data/comments"; // dummy data
+//import comments from "../../data/comments"; // dummy data
 import CommentElement from "./CommentElement";
-import { getComments } from "../../apis/api";
+import { getComments, createComment, getUser } from "../../apis/api";
+import { getCookie } from "../../utils/cookie";
 
 const Comment = ({ postId }) => {
   const [commentList, setCommentList] = useState([]); // state for comments
   const [newContent, setNewContent] = useState(""); // state for new comment
+  const [isLoggedIn, setIsLoggedIn] = useState();
 
   useEffect(() => {
     const getCommentsAPI = async () => {
-      const comments = await getComments();
+      const comments = await getComments(postId);
       setCommentList(comments);
     };
     getCommentsAPI();
+    const loggedIn = getCookie("access_token") ? true : false;
+    setIsLoggedIn(loggedIn);
   }, []);
 
   const handleCommentSubmit = (e) => {
     e.preventDefault();
-    setCommentList([
-      // TODO: add api call for creating comment
-      ...commentList,
-      {
-        id: commentList.length + 1,
-        content: newContent,
-        created_at: new Date().toISOString(),
-        post: postId,
-        author: {
-          id: 1,
-          username: "user1",
-        },
-      },
-    ]);
-    console.log({
-      post: postId,
-      content: newContent,
-    });
+    createComment({ post: postId, content: newContent });
     setNewContent("");
   };
 
@@ -56,23 +43,26 @@ const Comment = ({ postId }) => {
           />
         );
       })}
-
-      <form
-        className="flex flex-row mt-10 gap-3"
-        onSubmit={handleCommentSubmit}
-      >
-        <input
-          type="text"
-          value={newContent}
-          placeholder="댓글을 입력해주세요"
-          className="input"
-          style={{ width: "calc(100% - 100px)" }}
-          onChange={(e) => setNewContent(e.target.value)}
-        />
-        <button type="submit" className="button">
-          작성
-        </button>
-      </form>
+      {getCookie("access_token") ? (
+        <div>
+          <form
+            className="flex flex-row mt-10 gap-3"
+            onSubmit={handleCommentSubmit}
+          >
+            <input
+              type="text"
+              value={newContent}
+              placeholder="댓글을 입력해주세요"
+              className="input"
+              style={{ width: "calc(100% - 100px)" }}
+              onChange={(e) => setNewContent(e.target.value)}
+            />
+            <button type="submit" className="button">
+              작성
+            </button>
+          </form>
+        </div>
+      ) : null}
     </div>
   );
 };
